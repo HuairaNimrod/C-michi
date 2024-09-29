e1a7acd115e7c2f8759a1fdb124cd88a5aeb4fd7
Nimrod Huaira <nimax29@gmail.com>
2024-09-29 17:02:22 -0600

    dynamic array

---
+++ 02-divisors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Divisors {
+
+    class Program{
+
+        static void Main(string[] args){
+            Console.WriteLine("Programs that prints the divisors of a number, except the number, in an Dynamic Array ");
+            Console.Write("number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            List<int> numi = new List<int>();
+            for (int i = 1; i < number; i++)
+            {
+                if(number%i==0){
+                    numi.Add(i);
+                }                
+            }
+
+            foreach(var num in numi)
+            {
+                Console.Write(num+ " ");
+            }
+        }
+    }
+}
